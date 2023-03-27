6adb68a124744a5990b4ade6432c55a8a3e444ea
Batuhan <batuhanarik123@gmail.com>
2023-03-27 12:54:01 +0300

    some design changes made and project finished.

--- src/components/EntryCards.js
+++ src/components/EntryCards.js
@@ -1,7 +1,6 @@
 import EntryCard from "./EntryCard";
 
 const EntryCards = ({ filteredEntries }) => {
-    console.log(filteredEntries);
   return (
     <div className="flex flex-wrap m-3 mb-16 justify-center">
       {filteredEntries.map((entry, idx) => {
